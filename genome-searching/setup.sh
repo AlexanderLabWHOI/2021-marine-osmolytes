f1c7496238686e990fc93634b336a055edc5a54f
Harriet Alexander <harriet.xander@gmail.com>
2020-10-26 17:13:15 -0400

    updated setup

---
+++ genome-searching/setup.sh
@@ -0,0 +1,5 @@
+#set up environment: gtotree (add an environment file)
+
+# Install MarRef genomes from NCBI with bit toolkit:
+bit-dl-ncbi-assemblies -w MarRef_DBs/MarRef_accessions_GenBank_only.txt -f protein -j 4
+
