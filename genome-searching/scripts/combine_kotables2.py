bf013a34b69fc89b8a82b86719250ce20cce7659
Harriet Alexander <harriet.xander@gmail.com>
2020-11-02 18:38:54 -0500

    updated

--- genome-searching/scripts/combine_kotables2.py
+++ genome-searching/scripts/combine_kotables2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import pandas as pd
 import glob
 import os
@@ -8,7 +9,8 @@ KO_index = pd.read_csv(sys.argv[2], sep='\t', index_col=0).index
 
 out_df = pd.DataFrame(columns=KO_index)
 header = sys.argv[4]
-for csv_file in glob.glob(sys.argv[3]+header+'*csv'):
+for csv_file in glob.glob(os.path.join(sys.argv[3],header+'*csv')):
+    print(csv_file) 
     df = pd.read_csv(csv_file, sep='\t', index_col=0)
     name = os.path.basename(csv_file).strip('.csv')
     for k in KO_index:
